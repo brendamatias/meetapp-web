5c2a981121c99c5176e0d99a38f341c9010f31a8
Brenda Matias <brendamatias.sobral@gmail.com>
2019-10-07 22:38:15 -0300

    Dashboard finalizada

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
+import {
+  MdChevronLeft,
+  MdChevronRight,
+  MdSentimentDissatisfied,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+
+import { Container, MeetupCard, NoMeetup, Pagination, Loading } from './styles';
+
+const formatDate = d =>
+  format(d, "dd ' de ' MMMM ', às ' H'h'", { locale: pt });
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  const [loading, setLoading] = useState(false);
+  const [meetups, setMeetups] = useState([]);
+
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    async function loadMeetups() {
+      const response = await api.get('organizing', {
+        params: {
+          page,
+        },
+      });
+
+      const data = response.data.map(meetup => ({
+        ...meetup,
+        formattedDate: formatDate(parseISO(meetup.date)),
+      }));
+
+      setMeetups(data);
+      setLoading(false);
+    }
+
+    loadMeetups();
+  }, [page]);
+
+  async function handlePageChange(next) {
+    const nextPage = page + (next ? 1 : -1);
+
+    setPage(nextPage);
+  }
+
+  return (
+    <Container>
+      <header>
+        <strong>Meus meetups</strong>
+        <button type="button">Novo meetup</button>
+      </header>
+      <nav>
+        {!loading ? (
+          <>
+            {meetups.length > 0 ? (
+              <ul>
+                {meetups.map(meetup => (
+                  <MeetupCard key={String(meetup.id)}>
+                    <Link to={`details/${meetup.id}`}>{meetup.title}</Link>
+                    <time>{meetup.formattedDate}</time>
+                  </MeetupCard>
+                ))}
+              </ul>
+            ) : (
+              <NoMeetup>
+                <MdSentimentDissatisfied color="#fff" size={40} />
+                <span>Que pena, você não tem nenhum meetup.</span>
+              </NoMeetup>
+            )}
+
+            <Pagination>
+              {page > 1 && (
+                <button onClick={() => handlePageChange(false)} type="button">
+                  <MdChevronLeft color="#fff" size={30} />
+                </button>
+              )}
+              <span>{page}</span>
+
+              {meetups.length > 0 && (
+                <button onClick={() => handlePageChange(true)} type="button">
+                  <MdChevronRight color="#fff" size={30} />
+                </button>
+              )}
+            </Pagination>
+          </>
+        ) : (
+          <Loading>Carregando...</Loading>
+        )}
+      </nav>
+    </Container>
+  );
 }
